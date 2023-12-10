f75f5f841f27f31b4309363bab3ce4860077f10c
EWHernandez <erhe3221@colorado.edu>
2023-12-09 20:12:57 -0700

    app created ready for postGres test

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import psycopg2 as pg
+import create_exercise_input_db as ceid
 
+from flask import Flask
+from flask import render_template
+from flask import request, redirect, url_for, flash, jsonify
+
+app = Flask(__name__)
+app.secret_key = 'team_3_rules' 
+
+db_args = {
+    'dsn': "postgres://exercise_input_user:UJBoq6dkr6d26DEF14q4vVShnKJ4u6Xz@dpg-clqd7kae9h4c73ald8qg-a.oregon-postgres.render.com/exercise_input"
+}
+
+@app.route('/') #default
 def create_workouts():
     # Create the database and establish a connection
     conn = pg.connect(**db_args)
@@ -21,6 +34,8 @@ def create_workouts():
     # Commit changes and close the connection
     conn.commit()
     conn.close()
+
+    return render_template('exercise_input.html')
 
 # Function to add a workout entry
 #modify to have user Id
@@ -56,18 +71,26 @@ def get_workouts():
 
     return rows
 
-# Function to delete a user entry by id
-def delete_entry(exercise_name):
-    conn = pg.connect(**db_args)
-    cursor = conn.cursor()
+@app.route('/testinput', methods=['POST'])
+def testinput():
+    #Get user iD follow syntax
+    name = request.form.get('exercise_name')
+    etype = request.form.get('exercise_type')
+    minute = request.form.get('exercise_minute')
+    note = request.form.get('exercise_notes')
+    date = request.form.get('dateinput')
 
-    # Delete data from the "workouts" table
-    cursor.execute('DELETE FROM workouts WHERE exercise_name=%s', (exercise_name,))
+    #modify to include userID
+    ceid.add_workout(date, name, minute, etype, note)
+    
+    print("made it here")
+    return redirect('/', code=302)
 
-    # Commit changes and close the connection
-    conn.commit()
-    conn.close()
+@app.route('/populate_table', methods=['GET'])
+def populate_table():
+    return jsonify(ceid.get_workouts())
 
+@app.route('/empty_table', methods=['GET'])
 def recreateTable():
     conn = pg.connect(**db_args)
     cursor = conn.cursor()
@@ -86,8 +109,5 @@ def recreateTable():
         )
     ''')
 
-
-
-# Example usage:
-# add_workout('Bench Press', '12/1/23', '15', 'strength', '3 sets of 12 reps')
-# delete_entry(Bench Press)
+    print("Table emptied")
+    return redirect('/', code=302)
